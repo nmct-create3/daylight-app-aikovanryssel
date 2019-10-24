6296b3845f5b912a73f311f6f9cb29c716f0b61f
aiko <aiko.van.ryssel@student.howest.be>
2019-10-24 19:56:56 +0200

    zon verplaatsen lukt nog niet

--- script/app.js
+++ script/app.js
@@ -1,5 +1,13 @@
+// Add a few headers - UITZONDERING
+let domLocatie,
+	lat = 50.8027841,
+	lon = 3.2097454,
+	customHeaders = new Headers();
+customHeaders.append('Accept', 'application/json');
+
 // _ = helper functions
 function _parseMillisecondsIntoReadableTime(timestamp) {
+	console.log(timestamp);
 	//Get hours from milliseconds
 	const date = new Date(timestamp * 1000);
 	// Hours part from the timestamp
@@ -16,7 +24,8 @@ function _parseMillisecondsIntoReadableTime(timestamp) {
 // 5 TODO: maak updateSun functie
 
 // 4 Zet de zon op de juiste plaats en zorg ervoor dat dit iedere minuut gebeurt.
-let placeSunAndStartMoving = (totalMinutes, sunrise) => {
+const placeSunAndStartMoving = (totalMinutes, minLeft) => {
+	console.log(totalMinutes, minLeft);
 	// In de functie moeten we eerst wat zaken ophalen en berekenen.
 	// Haal het DOM element van onze zon op en van onze aantal minuten resterend deze dag.
 	// Bepaal het aantal minuten dat de zon al op is.
@@ -27,25 +36,71 @@ let placeSunAndStartMoving = (totalMinutes, sunrise) => {
 	// Bekijk of de zon niet nog onder of reeds onder is
 	// Anders kunnen we huidige waarden evalueren en de zon updaten via de updateSun functie.
 	// PS.: vergeet weer niet om het resterend aantal minuten te updaten en verhoog het aantal verstreken minuten.
+
+	let timeLeftHTML = '';
+	timeLeftHTML = `<time class="c-horizon__time js-sunrise">${minLeft}</time>`;
+	domTimeLeft.innerHTML = timeLeftHTML;
 };
 
 // 3 Met de data van de API kunnen we de app opvullen
-let showResult = queryResponse => {
+const showResult = queryResponse => {
 	// We gaan eerst een paar onderdelen opvullen
 	// Zorg dat de juiste locatie weergegeven wordt, volgens wat je uit de API terug krijgt.
 	// Toon ook de juiste tijd voor de opkomst van de zon en de zonsondergang.
 	// Hier gaan we een functie oproepen die de zon een bepaalde positie kan geven en dit kan updaten.
 	// Geef deze functie de periode tussen sunrise en sunset mee en het tijdstip van sunrise.
+	console.log(queryResponse.city.name);
+	sunrise = _parseMillisecondsIntoReadableTime(queryResponse.city.sunrise);
+	sunset = _parseMillisecondsIntoReadableTime(queryResponse.city.sunset);
+	console.log(sunrise);
+	console.log(sunset);
+
+	let locationHTML = '';
+	locationHTML = `<p class="c-app__location u-muted js-location">
+	${queryResponse.city.name}, ${queryResponse.city.country}	</p>`;
+	domLocatie.innerHTML = locationHTML;
+
+	let sunsetHTML = '';
+	sunsetHTML = `<time class="c-horizon__time js-sunset">${sunset}</time>`;
+	domSunset.innerHTML = sunsetHTML;
+
+	let sunriseHTML = '';
+	sunriseHTML = `<time class="c-horizon__time js-sunrise">${sunrise}</time>`;
+	domSunrise.innerHTML = sunriseHTML;
+
+
+	const test = new Date();
+	
+	//console.log('test ' + test.getHours()*60);
+	let totalTime= test.getHours()*60+test.getMinutes();
+	let timeSunset=((parseInt(sunset[0]+sunset[1])*60)+parseInt(sunset[3]+sunset[4]))
+	let minLeft= timeSunset-totalTime
+	placeSunAndStartMoving(totalTime,minLeft);
 };
 
 // 2 Aan de hand van een longitude en latitude gaan we de yahoo wheater API ophalen.
-let getAPI = (lat, lon) => {
+const getAPI = async (lat, lon) => {
 	// Eerst bouwen we onze url op
 	// Met de fetch API proberen we de data op te halen.
 	// Als dat gelukt is, gaan we naar onze showResult functie.
-};
+	const SERVER_ENDPOINT = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=aa68f50ed970b463bc2b0825c3978f77&units=metric&lang=nl&cnt=1`;
+	const data = await fetchData(SERVER_ENDPOINT);
 
+	showResult(data);
+	console.log(data);
+};
+const fetchData = url => {
+	return fetch(url).then(r => r.json());
+	/* 		.then(kip=>{showResult(kip)});
+	 */
+};
 document.addEventListener('DOMContentLoaded', function() {
 	// 1 We will query the API with longitude and latitude.
-	getAPI(50.8027841, 3.2097454);
+	//appid=aa68f50ed970b463bc2b0825c3978f77
+	domLocatie = document.querySelector('.js-location');
+	domSunset = document.querySelector('.js-sunset');
+	domSunrise = document.querySelector('.js-sunrise');
+	domTimeLeft= document.querySelector('.js-time-left');
+
+	getAPI(lat, lon);
 });
